f899fa9b6835cfc5a46fc5842b8c942c5b3fe45a
caiofilipemr <caiofilipemr@gmail.com>
2017-12-13 19:28:13 -0200

    método de checar variavel implementado e testado

--- compile.js
+++ compile.js
@@ -2,6 +2,7 @@ var error = false;
 var automato = [];
 var estadoInicial;
 var estadoAtual;
+var tabelaSimbolos;
 
 function Estado(tokens, msgErro, lerTokenEspecial, acaoSemantica) {
     this.tokens = tokens;
@@ -9,8 +10,10 @@ function Estado(tokens, msgErro, lerTokenEspecial, acaoSemantica) {
     this.lerTokenEspecial = lerTokenEspecial || function(token) { return false; };
     this.acaoSemantica = acaoSemantica || function(token) {};
     this.lerToken = function (token) {
-        if (tokens[token] || this.lerTokenEspecial(token)) {
-            estadoAtual = automato[tokens[token] || this.lerTokenEspecial(token)];
+        if (tokens[token]) {
+            estadoAtual = automato[tokens[token]];
+        } else if (this.lerTokenEspecial(token)) {
+            estadoAtual = automato[this.lerTokenEspecial(token)];
             this.acaoSemantica(token);
         } else {
             error = true;
@@ -19,26 +22,34 @@ function Estado(tokens, msgErro, lerTokenEspecial, acaoSemantica) {
 }
 
 function checarVariavel(token) {
-    console.log(/([a-z]|[A-Z])([a-z]|[A-Z]|[0-9])*$/.test(token));
-    if (token.search("([a-z]|[A-Z])([a-z]|[A-Z]|[0-9])*$")) {
+    var m = token.match(/[a-zA-Z][a-zA-Z0-9]*\$/);
+    console.log(m);
+    if (m.length == 1 && m == token) {
         return 3;
     } else {
         return false;
     }
 }
 
+function adicionarNaTabelaDeSimbolos(token) {
+    tabelaSimbolos[token] = true;
+}
+
 estadoInicial = new Estado({ 'programa': 1 }, 'Esperado ID=programa');
 
 automato.push(estadoInicial);
 automato.push(new Estado({ 'var': 2 }, 'Esperado ID=var'));
-automato.push(new Estado({}, 'Esperado ID=;', checarVariavel));
+automato.push(new Estado({ ';': 4 }, 'Esperado ID=;', checarVariavel, adicionarNaTabelaDeSimbolos));
 automato.push(new Estado({ ';': 4 }, 'Esperado ID=;'));
 automato.push(new Estado({ 'leia': 5 }, 'Esperado ID=leia'));
 
 function compilar() {
     estadoAtual = estadoInicial;
+    tabelaSimbolos = {};
+    error = false;
     var str = document.getElementById('txtEditor').value;
     var tokens = str.split(RegExp(' |\\n|\\s|\\r'));
+
     for (token of tokens) {
         estadoAtual.lerToken(token);
         if (error) {
